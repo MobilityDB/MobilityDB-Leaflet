7cd18eab6ac32e235800abd4a1f0947a027f35ee
flobaudry <boblennon24@gmail.com>
2023-04-18 01:31:57 +0200

    last update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import LVectorGrid from "./components/LVectorGrid";
+import RLVectorGrid from "./components/RLVectorGrid";
+import {MapContainer} from "react-leaflet";
 
 function App() {
   return (
-    <div className={'App'}>
-      <LVectorGrid />
+    <div className={'App notransition'}>
+  <LVectorGrid />
     </div>
   );
 }
--- src/components/LVectorGrid.jsx
+++ src/components/LVectorGrid.jsx
@@ -1,87 +1,127 @@
 import React, { useRef, useEffect, useState } from "react";
 import L from "leaflet";
 import "leaflet.vectorgrid";
+import {remove} from "leaflet/src/dom/DomUtil";
 
 export default function LVectorGrid() {
   const mapRef = useRef(null);
+  const [startSimulation, setStartSimulation] = useState(false);
   const [timez, setTimez] = useState("1970-01-01 7:00:00");
   const [vectorTileLayer, setVectorTileLayer] = useState(null);
+  const [limit, setLimit] = useState(100)
+  const [currentTime, setCurrentTime] = useState(Date.now())
+  const [fps, setFps] = useState(0)
 
   useEffect(() => {
     const map = L.map("map").setView([50.5, 4], 8);
-
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
       maxZoom: 18,
     }).addTo(map);
 
-    setVectorTileLayer(
-      L.vectorGrid.protobuf(
-        `http://192.168.0.171:7802/public.get_reduced_tilelayer/{z}/{x}/{y}.pbf?timez=${timez}`,
-        {
-          rendererFactory: L.canvas.tile,
-          vectorTileLayerStyles: {
-            reduced: function (properties, zoom) {
-              return {
-                radius: 5,
-                weight: 3,
-                fill: true,
-                fillColor: "red",
-                fillOpacity: 1,
-                color: "black",
-              };
-            },
-          },
-        }
-      ).addTo(map)
-    );
 
     mapRef.current = map;
 
-    // clean up the map instance when the component unmounts
+    const newLayer = L.vectorGrid.protobuf(
+      `http://192.168.0.171:7802/public.get_reduced_tilelayer/{z}/{x}/{y}.pbf?maxpoints=${limit}&timez=${timez}`,
+      {
+        opacity: 1,
+        rendererFactory: L.svg.tile,
+        vectorTileLayerStyles: {
+          reduced: function (properties, zoom) {
+            return {
+              radius: 2,
+              weight: 2,
+              fill: true,
+              fillColor: "black",
+              fillOpacity: 1,
+              color: "black",
+            };
+          },
+        },
+      }
+    ).addEventListener('loading', ()=>console.log('loading')).addTo(mapRef.current);
+    setVectorTileLayer(newLayer);
+
+
     return () => {
       map.remove();
     };
   }, []);
 
   function updateTimez() {
-    function sleep(number) {
-return new Promise((resolve) => setTimeout(resolve, number));
-    }
-
-    sleep(100).then(() => {
-      setTimez((timez) => {
-        const date = new Date(timez);
-        date.setMinutes(date.getMinutes() + 1);
-        date.setHours(date.getHours() + 1);
-        if (date.getHours() >= 23) {
-          date.setMinutes(0);
-          date.setHours(7);
-        }
-        return date.toISOString().slice(0, 19).replace("T", " ");
-      });
+    setTimez((timez) => {
+      const date = new Date(timez);
+      date.setSeconds(date.getSeconds() + 10);
+      date.setHours(date.getHours() + 1);
+      if (date.getHours() >= 23) {
+        date.setMinutes(0);
+        date.setHours(7);
+      }
+      return date.toISOString().slice(0, 19).replace("T", " ");
     });
   }
 
+  async function removeLayer(tmp, updateTime) {
+    console.log('remove layer')
+    const now = Date.now()
+    setFps(Math.round(1000/(now-currentTime)))
+    setCurrentTime(now)
+    setInterval(()=> {tmp.remove()}, 500)
+    if (updateTime) updateTimez()
+  }
+
+  async function extracted(updateTime = true, vectorTileLayer) {
+    if (!vectorTileLayer.isLoading()){
+    const newLayer = L.vectorGrid.protobuf(
+      `http://192.168.0.171:7802/public.get_reduced_tilelayer/{z}/{x}/{y}.pbf?maxpoints=${limit}&timez=${timez}`,
+      {
+        opacity: 1,
+        rendererFactory: L.canvas.tile,
+        vectorTileLayerStyles: {
+          reduced: function (properties, zoom) {
+            return {
+              radius: 2,
+              weight: 2,
+              fill: true,
+              fillColor: "black",
+              fillOpacity: 1,
+              color: "black",
+            };
+          },
+        },
+      }
+    ).addTo(mapRef.current).addEventListener('load', ()=>removeLayer(vectorTileLayer, updateTime));
+    setVectorTileLayer(newLayer);
+    }
+
+  }
+
+  useEffect(() => {
+    if (startSimulation) {
+      extracted(true, vectorTileLayer);
+    }
+  }, [timez, startSimulation, limit, vectorTileLayer]);
+
   useEffect(() => {
     if (vectorTileLayer) {
-      vectorTileLayer.setUrl(
-        `http://192.168.0.171:7802/public.get_reduced_tilelayer/{z}/{x}/{y}.pbf?timez=${timez}`
-      );
-      updateTimez();
+    extracted(false, vectorTileLayer)
     }
-  }, [timez]);
+  }, [limit, vectorTileLayer]);
 
   return (
     <div style={{ height: "100%", position: "relative" }}>
-      <div id={"map"} style={{ height: "75%", position: "relative" }}></div>
-      <button
-        onClick={updateTimez}
-      >
+      <div id="map" style={{ height: "75%", position: "relative" }}></div>
+      <button onClick={() => setStartSimulation(!startSimulation)}>
         start simulation
       </button>
+      <button onClick={() => setTimez("1970-01-01 7:00:00")}>
+        reset time
+      </button>
+      <input type='number' value={limit} onChange={(e)=>setLimit(e.target.value)}/>
       <div>{timez}</div>
+      <div>{fps}</div>
     </div>
   );
 }
