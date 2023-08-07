419b35c04328a2f523cfc31c0394aa3da8ba8913
flobaudry <33914132+flobaudry@users.noreply.github.com>
2023-08-07 21:49:17 +0200

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -1,20 +1,22 @@
-drop function tripsfct(z integer, x integer, y integer, maxpoints integer);
+SELECT asMFJSON(transform(trip, 4326))::json FROM ships limit 1;
+
+
+drop function if exists tripsfct(z integer, x integer, y integer, maxpoints integer);
 create FUNCTION public.tripsfct(
             z integer, x integer, y integer, maxpoints integer default 10)
 RETURNS bytea
 AS $$
     WITH bounds AS (
-        SELECT ST_TileEnvelope(z, x, y) AS geom
+        SELECT ST_TileEnvelope(z, x, y)::stbox AS geom
     ),
     val AS (
-        SELECT id,  asMVTGeom(fullday_trajectory, (bounds.geom)::stbox) as geom_times
-        FROM persona_small_2000, bounds
-        ORDER BY id
+        SELECT mmsi,  asMVTGeom(trip_3857, bounds.geom) as geom_times -- delete transform
+        FROM ships, bounds
+        ORDER BY mmsi
         LIMIT maxpoints
-
     ),
     mvtgeom AS(
-        SELECT id, (geom_times).geom, (geom_times).times
+        SELECT mmsi, (geom_times).geom, (geom_times).times
         FROM val
     )
     SELECT st_asmvt(mvtgeom, 'reduced') FROM mvtgeom
@@ -24,9 +26,93 @@ STABLE
 PARALLEL SAFE;
 
 
-SELECT asMFJSON(transform(fullday_trajectory, 4326))::json FROM persona_small_2000 limit 1;
+CREATE OR REPLACE FUNCTION input_ais()
+RETURNS text AS $$
+BEGIN
 
-update persona_small set fullday_trajectory = transform(fullday_trajectory, 3857);
+  DROP TABLE IF EXISTS AISInput;
+  CREATE TABLE AISInput(
+    T timestamp,
+    TypeOfMobile varchar(100),
+    MMSI integer,
+    Latitude float,
+    Longitude float,
+    navigationalStatus varchar(100),
+    ROT float,
+    SOG float,
+    COG float,
+    Heading integer,
+    IMO varchar(100),
+    Callsign varchar(100),
+    Name varchar(100),
+    ShipType varchar(100),
+    CargoType varchar(100),
+    Width float,
+    Length float,
+    TypeOfPositionFixingDevice varchar(100),
+    Draught float,
+    Destination varchar(100),
+    ETA varchar(100),
+    DataSourceType varchar(100),
+    SizeA float,
+    SizeB float,
+    SizeC float,
+    SizeD float,
+    Geom geometry(Point, 4326)
+  );
 
-create index persona_small_reduced_geom_idx on persona_small_2000 using gist (fullday_trajectory);
+  RAISE INFO 'Reading CSV files ...';
 
+  COPY AISInput(T, TypeOfMobile, MMSI, Latitude, Longitude, NavigationalStatus,
+    ROT, SOG, COG, Heading, IMO, CallSign, Name, ShipType, CargoType, Width, Length,
+    TypeOfPositionFixingDevice, Draught, Destination, ETA, DataSourceType,
+    SizeA, SizeB, SizeC, SizeD)
+  FROM '/mnt/g/ais_datasets/aisdk-2023-05-23/aisdk-2023-05-23.csv' DELIMITER ',' CSV HEADER;
+
+  RAISE INFO 'Updating AISInput table ...';
+
+  UPDATE AISInput SET
+    NavigationalStatus = CASE NavigationalStatus WHEN 'Unknown value' THEN NULL END,
+    IMO = CASE IMO WHEN 'Unknown' THEN NULL END,
+    ShipType = CASE ShipType WHEN 'Undefined' THEN NULL END,
+    TypeOfPositionFixingDevice = CASE TypeOfPositionFixingDevice
+    WHEN 'Undefined' THEN NULL END,
+    Geom = ST_SetSRID( ST_MakePoint( Longitude, Latitude ), 4326);
+
+  RAISE INFO 'Creating AISInputFiltered table ...';
+
+  DROP TABLE IF EXISTS AISInputFiltered;
+  CREATE TABLE AISInputFiltered AS
+  SELECT DISTINCT ON(MMSI,T) *
+  FROM AISInput
+  WHERE Longitude BETWEEN -16.1 and 32.88 AND Latitude BETWEEN 40.18 AND 84.17;
+
+  RAISE INFO 'Creating Ships table ...';
+
+  -- DROP TABLE IF EXISTS Ships;
+  -- CREATE TABLE Ships(MMSI, Trip, SOG, COG) AS
+  -- SELECT MMSI,
+    -- tgeompoint_seq(array_agg(tgeompoint_inst(ST_Transform(Geom, 25832), T) ORDER BY T)),
+    -- tfloat_seq(array_agg(tfloat_inst(SOG, T) ORDER BY T) FILTER (WHERE SOG IS NOT NULL)),
+    -- tfloat_seq(array_agg(tfloat_inst(COG, T) ORDER BY T) FILTER (WHERE COG IS NOT NULL))
+  -- FROM AISInputFiltered
+  -- GROUP BY MMSI;
+
+  DROP TABLE IF EXISTS Ships7;
+  CREATE TABLE Ships7(MMSI, Trip) AS
+  SELECT MMSI, tgeompoint_seqset_gaps(
+    array_agg(tgeompoint_inst(ST_Transform(Geom, 25832), T) ORDER BY T),
+    interval '1 hour')
+  FROM AISInputFiltered
+  GROUP BY MMSI;
+
+  ALTER TABLE Ships7 ADD COLUMN Traj geometry;
+  UPDATE Ships7 SET Traj = trajectory(Trip);
+
+    alter table ships7 add column trip_3857 tgeompoint;
+    update ships7 set trip_3857 = transform(trip, 3857);
+    update ships7 set trip = transform(trip, 4326);
+
+  RETURN 'The End';
+END;
+$$ LANGUAGE 'plpgsql' STRICT;
